b2ffb28db46106f6d288dbf3e09a2337b26c4ba8
censorjam <jones.marc@gmail.com>
2016-07-05 17:16:28 +0100

    commit

---
+++ TestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ruley.Core;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var rm = new RuleManager();
+            rm.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
